9b020ab65cee48c35f8b76fe4ca934e0bdb2a72d
wim <wfjvdham@gmail.com>
2017-12-12 11:34:54 -0500

    added ranking for departements and updated the dataset

--- data/script.R
+++ data/script.R
@@ -6,13 +6,19 @@ recetas_ing <- read_excel('./data/recetas-ing.xlsx')
 
 #summary(recetas)
 
+dedeptoRank <- data_frame(
+  depto = unique(recetas$depto)
+) %>%
+  mutate(deptoRank = 1:nrow(.))
+
 recetas <- recetas %>%
   mutate(tiempo_mins = ifelse(is.na(tiempo_mins) & !is.na(tiempo_dias), 0, tiempo_mins),
          tiempo_dias = ifelse(is.na(tiempo_dias) & !is.na(tiempo_mins), 0, tiempo_dias),
          tiempo_mins = tiempo_mins + tiempo_dias * 24 * 60,
          price = runif(nrow(recetas), 1, 100)) %>%
   left_join(recetas_ing, by = 'uid') %>%
-  select(uid, price, name = name.x, region, depto, instruc, dificultad, tiempo_mins, ing)
+  select(uid, price, name = name.x, region, depto, instruc, dificultad, tiempo_mins, ing) %>%
+  left_join(dedeptoRank, by = 'depto')
 
 saveRDS(recetas, file = "data/recetas.Rda")
 
